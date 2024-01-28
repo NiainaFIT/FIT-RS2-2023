47f1af5c2bfbcc395b5838b874862cba42052c82
Niaina Sadiković-Ratsimanohatra <niaina.s.r@edu.fit.ba>
2024-01-28 15:44:56 +0100

    Database scaffolding
    Schaffolded with  Scaffold-DbContext "Data Source=SOMEPC;Initial Catalog=eProdaja; user=sa; password=test; TrustServerCertificate=true" Microsoft.EntityFrameworkCore.SqlServer -NoPluralize -ContextDir "Database" -OutputDir "Entities" -Namespace "eProdaja.Infrastructure.Entities" -ContextNamespace "eProdaja.Infrastructure.Database"

---
+++ eProdaja/eProdaja.Infrastructure/Entities/JediniceMjere.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace eProdaja.Infrastructure.Entities;
+
+public partial class JediniceMjere
+{
+    public int JedinicaMjereId { get; set; }
+
+    public string Naziv { get; set; } = null!;
+
+    public virtual ICollection<Proizvodi> Proizvodi { get; set; } = new List<Proizvodi>();
+}
---
+++ eProdaja/eProdaja.Infrastructure/Entities/Proizvodi.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace eProdaja.Infrastructure.Entities;
+
+public partial class Proizvodi
+{
+    public int ProizvodId { get; set; }
+
+    public string Naziv { get; set; } = null!;
+
+    public string Sifra { get; set; } = null!;
+
+    public decimal Cijena { get; set; }
+
+    public int VrstaId { get; set; }
+
+    public int JedinicaMjereId { get; set; }
+
+    public byte[]? Slika { get; set; }
+
+    public byte[]? SlikaThumb { get; set; }
+
+    public bool Status { get; set; }
+
+    public virtual ICollection<IzlazStavke> IzlazStavke { get; set; } = new List<IzlazStavke>();
+
+    public virtual JediniceMjere JedinicaMjere { get; set; } = null!;
+
+    public virtual ICollection<NarudzbaStavke> NarudzbaStavke { get; set; } = new List<NarudzbaStavke>();
+
+    public virtual ICollection<Ocjene> Ocjene { get; set; } = new List<Ocjene>();
+
+    public virtual ICollection<UlazStavke> UlazStavke { get; set; } = new List<UlazStavke>();
+
+    public virtual VrsteProizvoda Vrsta { get; set; } = null!;
+}
